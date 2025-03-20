7d0646c3c559aa217a9886d2123f71007bb8bfdd
Zero Y One K <markbillhv@gmail.com>
2025-03-20 19:57:05 +0700

    Update redux.tsx

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -80,7 +80,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore | null>(null);
+  const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
